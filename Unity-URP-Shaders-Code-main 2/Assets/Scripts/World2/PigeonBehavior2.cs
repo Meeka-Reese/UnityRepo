63283b750514b733d22ada7071ce466ea6ccf09e
Meeka-Reese <157521411+Meeka-Reese@users.noreply.github.com>
2025-04-06 12:55:14 -0400

    pigeon

--- Unity-URP-Shaders-Code-main 2/Assets/Scripts/World2/PigeonBehavior2.cs
+++ Unity-URP-Shaders-Code-main 2/Assets/Scripts/World2/PigeonBehavior2.cs
@@ -23,156 +23,273 @@ public class PigeonBehavior2 : MonoBehaviour
     private float MaxSpeed;
     private float turnForce = 200f;
     private float MinY = 0f;
-    private List<GameObject> Goals;
     private bool Wait = false;
-
+    private bool RanMove = false;
+    private bool GoalMove = false;
+    public int PigeonGroupNum;
+    private List<GameObject> GoalKeys;
     
 
 
 
     void Start()
     {
-        Goals = new List<GameObject>();
-        foreach (GameObject goal in GameObject.FindGameObjectsWithTag("Goal"))
-        {
-            Goals.Add(goal);
-        }
+        
         MaxSpeed = speed * 3;
         pigeonWave = transform.parent.GetComponent<PigeonWave2>();
         rb = GetComponent<Rigidbody>();
         rb.linearVelocity = new Vector3(speed, 0, 0);
+        GoalKeys = new List<GameObject>(PigeonWave2.Goals.Keys);
         StartCoroutine(RandomMove());
         
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        
-            Vector3 directionToTrigger = (other.transform.position - transform.position).normalized;
-            rb.AddForce(-directionToTrigger * MaxSpeed, ForceMode.Acceleration);
-        
-        
-
-    }
+    
 
     private void OnCollisionEnter(Collision collision)
     {
         Vector3 Normal = collision.contacts[0].normal;
         if (collision.gameObject.CompareTag("GoalCollide") && (Normal == Vector3.up))
         {
+            Vector3 directionToTrigger = Vector3.zero;
+            if ((collision.transform.position - transform.position) != Vector3.zero)
+            {
+                directionToTrigger = (collision.transform.position - transform.position).normalized;
+            }
+
+            rb.AddForce(-directionToTrigger * MaxSpeed, ForceMode.Acceleration);
+            PigeonWave2.Goals[collision.gameObject.transform.parent.gameObject]++;
             Wait = true;
             StartCoroutine(Waiting());
         }
     }
 
-    
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("GoalCollide"))
+        {
+            if (PigeonWave2.Goals[collision.gameObject.transform.parent.gameObject] > 0)
+            {
+                PigeonWave2.Goals[collision.gameObject.transform.parent.gameObject]--;
+            }
+
+            Wait = false;
+            StopCoroutine(Waiting());
+        }
+    }
+
+    void FixedUpdate() {
+        rb.linearVelocity *= 0.999f; // Small constant damping
+    }
 
     void Update()
     {
-        ClosestPigeon = PigeonWave2._pigeons[gameObject];
-        ClosestPigeonsClosest = PigeonWave2._pigeons[gameObject].gameObject.GetComponent<PigeonBehavior2>().ClosestPigeon;
-        //Asigning Pigeon Neighbors
-        
-        if (Wait)
+        if (float.IsNaN(rb.linearVelocity.x))
         {
-            rb.angularVelocity = Vector3.zero;
+            Debug.LogError("NaN detected! Resetting velocity.", this);
             rb.linearVelocity = Vector3.zero;
-            return;
         }
-        //Speed and Rotation kill if goal is reaching
-        
-            
-        if (ClosestPigeon != null)
+
+        if (PigeonGroupNum == 1)
         {
-            float distance = Vector3.Distance(transform.position, ClosestPigeon.transform.position);
-            if (distance < PigeonWave2.EqulibirumDist)
+            ClosestPigeon = PigeonWave2._pigeons[gameObject];
+        }
+        else if (PigeonGroupNum == 2)
+        {
+            ClosestPigeon = PigeonWave2._pigeons2[gameObject];
+        }
+
+        if (ClosestPigeon != null)
+            if (PigeonGroupNum == 1)
             {
+                ClosestPigeonsClosest = PigeonWave2._pigeons[gameObject].gameObject.GetComponent<PigeonBehavior2>()
+                    .ClosestPigeon;
+            }
+        else if (PigeonGroupNum == 2)
+        {
+            ClosestPigeonsClosest = PigeonWave2._pigeons2[gameObject].gameObject.GetComponent<PigeonBehavior2>()
+                .ClosestPigeon;
+        }
+
+        if (!RanMove || !GoalMove)
+            {
+            //Asigning Pigeon Neighbors
+
+            if (Wait)
+            {
+                rb.angularVelocity = Vector3.zero;
+                rb.linearVelocity = Vector3.zero;
+                return;
+            }
+
+
+            //Speed and Rotation kill if goal is reaching
+
+
+            if (ClosestPigeon != null)
+            {
+                float distance = Vector3.Distance(transform.position, ClosestPigeon.transform.position);
+                if (distance < PigeonWave2.EqulibirumDist)
+                {
+                    Vector3 diff = transform.position - ClosestPigeon.transform.position;
+                    diff.Normalize();
+                    diff /= distance;
+                    rb.linearVelocity += diff * DeltaMult;
+                }
+
+                if (distance > PigeonWave2.EqulibirumDist)
+                {
+                    Vector3 directionToPigeon = (ClosestPigeon.transform.position - transform.position).normalized;
+                    rb.AddForce(directionToPigeon * MaxSpeed, ForceMode.Acceleration);
+                }
+            }
+            //Balancing Pigeons based on EqulibriumDist
+
+            //Referencing Closest Pigeon 
+            if (ClosestPigeon != null)
+            rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, ClosestPigeon.GetComponent<Rigidbody>().linearVelocity,
+                Time.deltaTime * MaxSpeed);
+            if (ClosestPigeonsClosest != null)
+            {
+                rb.linearVelocity = Vector3.Lerp(rb.linearVelocity,
+                    ClosestPigeonsClosest.GetComponent<Rigidbody>().linearVelocity, Time.deltaTime * MaxSpeed);
+                if (PigeonGroupNum == 1)
+                {
+                    if ((PigeonWave2._pigeons[ClosestPigeonsClosest] != null) &&
+                        (PigeonWave2._pigeons[ClosestPigeonsClosest] != gameObject))
+                    {
+                        rb.linearVelocity = Vector3.Lerp(rb.linearVelocity,
+                            PigeonWave2._pigeons[ClosestPigeonsClosest].GetComponent<Rigidbody>().linearVelocity,
+                            Time.deltaTime * MaxSpeed);
+                    }
+                }
+                else if (PigeonGroupNum == 2)
+                {
+                    if ((PigeonWave2._pigeons2[ClosestPigeonsClosest] != null) &&
+                        (PigeonWave2._pigeons2[ClosestPigeonsClosest] != gameObject))
+                    {
+                        rb.linearVelocity = Vector3.Lerp(rb.linearVelocity,
+                            PigeonWave2._pigeons2[ClosestPigeonsClosest].GetComponent<Rigidbody>().linearVelocity,
+                            Time.deltaTime * MaxSpeed);
+                    }
+                }
+            }
+            
+
+            rb.linearVelocity = new Vector3(Mathf.Clamp(rb.linearVelocity.x, -MaxSpeed, MaxSpeed),
+                Mathf.Clamp(rb.linearVelocity.y, -MaxSpeed, MaxSpeed),
+                Mathf.Clamp(rb.linearVelocity.z, -MaxSpeed, MaxSpeed));
+            if (rb.linearVelocity.magnitude < speed / 3f)
+            {
+                Vector3 UpSpeed = rb.linearVelocity.normalized * speed;
+              
+                
+                rb.AddForce(UpSpeed, ForceMode.Acceleration);
+
+                
+            }
+            //Managing Speed Clamping and then making it random if it's too slow
+
+
+            Vector3 AdjustedPosition = new Vector3(transform.position.x, transform.position.y - pigeonWave.yHieght,
+                transform.position.z);
+            if (AdjustedPosition.magnitude > MaxDistance)
+            {
+                Vector3 directionToCenter = Vector3.zero;
+                if ((new Vector3(0, MinY + 50, 0) - transform.position) != Vector3.zero)
+                {
+                    directionToCenter = (new Vector3(0, MinY + 50, 0) - transform.position).normalized;
+                }
                 
 
-                Vector3 diff = transform.position - ClosestPigeon.transform.position;
-                diff.Normalize();
-                diff /= distance;
-                rb.linearVelocity += diff * DeltaMult;
+                rb.AddForce(directionToCenter * turnForce, ForceMode.Acceleration);
             }
+            //Making sure Pigeon doesn't stray too far from the world
+        }
 
-            if (distance > PigeonWave2.EqulibirumDist)
-            {
-                Vector3 directionToPigeon = (ClosestPigeon.transform.position - transform.position).normalized;
-                rb.AddForce(directionToPigeon * MaxSpeed, ForceMode.Acceleration);
-            }
-        }
-        //Balancing Pigeons based on EqulibriumDist
-        
-        //Referencing Closest Pigeon 
-        rb.linearVelocity = Vector3.Lerp(rb.linearVelocity,ClosestPigeon.GetComponent<Rigidbody>().linearVelocity, Time.deltaTime * MaxSpeed);
-        if (ClosestPigeonsClosest != null)
-        {
-            rb.linearVelocity = Vector3.Lerp(rb.linearVelocity,
-                ClosestPigeonsClosest.GetComponent<Rigidbody>().linearVelocity, Time.deltaTime * MaxSpeed);
-        }
-        
-        rb.linearVelocity = new Vector3(Mathf.Clamp(rb.linearVelocity.x, -MaxSpeed,MaxSpeed), 
-            Mathf.Clamp(rb.linearVelocity.y, -MaxSpeed,MaxSpeed), Mathf.Clamp(rb.linearVelocity.z, -MaxSpeed,MaxSpeed));
-        if (rb.linearVelocity.magnitude < speed/3f)
-        {
-            Vector3 RanPos = new Vector3(Random.Range(-MaxSpeed, MaxSpeed), Random.Range(-MaxSpeed, MaxSpeed), Random.Range(-MaxSpeed, MaxSpeed));
-            rb.linearVelocity += RanPos;
-        }   
-        //Managing Speed Clamping and then making it random if it's too slow
-        
-
-        Vector3 AdjustedPosition = new Vector3(transform.position.x, transform.position.y - pigeonWave.yHieght, transform.position.z);
-        if (AdjustedPosition.magnitude > MaxDistance)
-        {
-            Vector3 directionToCenter = (new Vector3(0,300,0) - transform.position).normalized;
-            rb.AddForce(directionToCenter * turnForce, ForceMode.Acceleration);
-        }
-        //Making sure Pigeon doesn't stray too far from the world
-        
     }
 
     private IEnumerator RandomMove()
     {
-        while(true)
+        while (true)
         {
+
             RanVal = Random.Range(0f, 1f);
+
             if (RanVal < .1f)
             {
+                Vector3 RanPos = new Vector3(Random.Range(-MaxSpeed / 2, MaxSpeed / 2),
+                    Random.Range(-MaxSpeed / 2, MaxSpeed / 2), Random.Range(-MaxSpeed / 2, MaxSpeed) / 2);
                 for (int i = 0; i < 10; i++)
                 {
-                    Vector3 RanPos = new Vector3(Random.Range(-MaxSpeed, MaxSpeed), Random.Range(-MaxSpeed, MaxSpeed), Random.Range(-MaxSpeed, MaxSpeed));
-                    rb.linearVelocity += RanPos;
-                    yield return new WaitForSeconds(.1f);
+
+                    RanMove = true;
+                    rb.AddForce(RanPos * 0.5f, ForceMode.Acceleration);
+                    yield return new WaitForSeconds(.2f);
+                    if (i == 9)
+                    {
+                        RanMove = false;
+                    }
                 }
+
             }
             //Random jult
 
-            if (RanVal > .7f)
+            if (RanVal > .8f)
             {
+                //GoalMove = true;
+                GameObject RandomKey = GoalKeys[Random.Range(0, GoalKeys.Count)];
+                if (PigeonWave2.Goals[RandomKey.gameObject] > 10)
+                {
+                    RandomKey = GoalKeys[Random.Range(0, GoalKeys.Count)];
+                    if (PigeonWave2.Goals[RandomKey.gameObject] > 10)
+                    {
+                        continue;
+                    }
+                }
+
+                //fix goals shit
+                Vector3 GoalVector = RandomKey.transform.position;
+                Vector3 directionToGoal = (GoalVector - transform.position);
+
+
                 for (int i = 0; i < 10; i++)
                 {
-                    int RandomInt = Random.Range(0, Goals.Count - 1);
-                    Vector3 MoveTowardsGoal = (Goals[RandomInt].transform.position - transform.position).normalized;
-                    if (Vector3.Distance(transform.position, Goals[RandomInt].transform.position) > 2f)
+                    if (directionToGoal != Vector3.zero)
                     {
-                        rb.linearVelocity = (MoveTowardsGoal * (turnForce / 2));
-                    }
-                    else
-                    {
-                        rb.linearVelocity = rb.linearVelocity * .95f;
-                        yield return new WaitForSeconds(.1f);
-                    }
+                        if (directionToGoal != Vector3.zero)
+                        {
+                            directionToGoal.Normalize();
 
-                    yield return new WaitForSeconds(.1f);
+
+                            if (Vector3.Distance(transform.position, GoalVector) > 2f)
+                            {
+                                rb.linearVelocity = directionToGoal * speed / 2;
+                            }
+                            else
+                            {
+                                rb.linearVelocity = rb.linearVelocity * .98f;
+                                yield return new WaitForSeconds(.1f);
+                            }
+
+
+                            if (i == 9)
+                            {
+                                GoalMove = false;
+                            }
+
+                            yield return new WaitForSeconds(.2f);
+                        }
+                    }
                 }
+                //Random move towards goal
+
+
+                yield return new WaitForSeconds(4f);
             }
-            //Random move towards goal
-            
-            
-            yield return new WaitForSeconds(4f);
+
         }
-        
-        
+
     }
 
     private IEnumerator Waiting()
--- Unity-URP-Shaders-Code-main 2/Assets/Scripts/World2/PigeonWave2.cs
+++ Unity-URP-Shaders-Code-main 2/Assets/Scripts/World2/PigeonWave2.cs
@@ -1,22 +1,36 @@
+using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PigeonWave2 : MonoBehaviour
 {
     [SerializeField] private GameObject _pigeonPrefab;
     private Vector3 _startPosition;
+    private Vector3 _startPosition2;
 
-    private float PigeonXNum = 10f;
+    [SerializeField] private float PigeonXNum = 10f;
 
     [SerializeField] public float yHieght = 20f;
-    public static float EqulibirumDist =  5f;
+    public static float EqulibirumDist =  10f;
 
     public static Dictionary<GameObject, GameObject> _pigeons = new Dictionary<GameObject, GameObject>();
+    public static Dictionary<GameObject, GameObject> _pigeons2 = new Dictionary<GameObject, GameObject>();
+    public static Dictionary<GameObject, int> Goals = new Dictionary<GameObject, int>();
    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        foreach (GameObject goal in GameObject.FindGameObjectsWithTag("Goal"))
+        {
+            Goals.Add(goal,0);
+        }
+
+        foreach (KeyValuePair<GameObject, int> pair in Goals)
+        {
+            Debug.Log(pair.Key.name);
+        }
         PigeonCreate();
     }
 
@@ -28,44 +42,129 @@ public class PigeonWave2 : MonoBehaviour
             {
                 _startPosition = new Vector3(i, yHieght, j);
                 GameObject _pigeon = Instantiate(_pigeonPrefab, _startPosition, Quaternion.identity);
+                _pigeon.GetComponent<PigeonBehavior2>().PigeonGroupNum = 1;
                 _pigeon.transform.SetParent(transform);
                 _pigeons.Add(_pigeon, null);
             }
         }
+        for (int i = 0; i < PigeonXNum; i++)
+        {
+            for (int j = 0; j < PigeonXNum; j++)
+            {
+                _startPosition2 = new Vector3(i + 10, yHieght, j + 10);
+                GameObject _pigeon = Instantiate(_pigeonPrefab, _startPosition2, Quaternion.identity);
+                _pigeon.GetComponent<PigeonBehavior2>().PigeonGroupNum = 2;
+                _pigeon.transform.SetParent(transform);
+                _pigeons2.Add(_pigeon, null);
+            }
+        }
+
+        StartCoroutine(ClosestPigeons());
     }
+
     
-    void Update()
+
+    private IEnumerator ClosestPigeons()
     {
         Dictionary<GameObject, GameObject> updates = new Dictionary<GameObject, GameObject>();
-        
-        foreach (var birdEntry in _pigeons)
+        Dictionary<GameObject, GameObject> updates2 = new Dictionary<GameObject, GameObject>();
+        while (true)
         {
-            GameObject bird = birdEntry.Key;
-            float closestDistance = float.MaxValue;
-            GameObject closestNeighbor = null;
 
-            foreach (var otherBirdEntry in _pigeons)
+            foreach (var birdEntry in _pigeons)
             {
-                GameObject otherBird = otherBirdEntry.Key;
-                
-                // Skip comparing with self
-                if (bird == otherBird) continue;
+                GameObject bird = birdEntry.Key;
+                float closestDistance = float.MaxValue;
+                GameObject closestNeighbor = null;
 
-                float distance = Vector3.Distance(bird.transform.position, otherBird.transform.position);
-                
-                if (distance < closestDistance)
+                GameObject RanBird = null;
+                foreach (var otherBirdEntry in _pigeons)
                 {
-                    closestDistance = distance;
-                    closestNeighbor = otherBird;
+                    GameObject otherBird = otherBirdEntry.Key;
+
+                    float Ran2 = Random.Range(0f, 1f);
+                    if (Ran2 < .1f)
+                    {
+                        RanBird = otherBirdEntry.Value;
+                    }
+
+
+                    // Skip comparing with self
+                    if (bird == otherBird) continue;
+
+                    float distance = Vector3.Distance(bird.transform.position, otherBird.transform.position);
+
+                    if (distance < closestDistance)
+                    {
+                        closestDistance = distance;
+                        // float Ran = Random.Range(0f, 1f);
+                        // if (Ran > 0.7f || RanBird == null)
+                        // {
+                        //     closestNeighbor = otherBird;
+                        // }
+                        // else
+                        // {
+                        //     closestNeighbor = RanBird;
+                        // }
+                        closestNeighbor = otherBird;
+                    }
                 }
+
+                updates[bird] = closestNeighbor;
             }
-            
-            updates[bird] = closestNeighbor;
-        }
-    
-        foreach (var update in updates)
-        {
-            _pigeons[update.Key] = update.Value;
+
+            foreach (var birdEntry in _pigeons2)
+            {
+                GameObject bird = birdEntry.Key;
+                float closestDistance = float.MaxValue;
+                GameObject closestNeighbor = null;
+                GameObject RanBird = null;
+                foreach (var otherBirdEntry in _pigeons2)
+                {
+                    GameObject otherBird = otherBirdEntry.Key;
+
+                    float Ran2 = Random.Range(0f, 1f);
+                    if (Ran2 < .1f)
+                    {
+                        RanBird = otherBirdEntry.Value;
+                    }
+
+
+                    // Skip comparing with self
+                    if (bird == otherBird) continue;
+
+                    float distance = Vector3.Distance(bird.transform.position, otherBird.transform.position);
+
+                    if (distance < closestDistance)
+                    {
+                        closestDistance = distance;
+                        // float Ran = Random.Range(0f, 1f);
+                        // if (Ran > 0.7f || RanBird == null)
+                        // {
+                        //     closestNeighbor = otherBird;
+                        // }
+                        // else
+                        // {
+                        //     closestNeighbor = RanBird;
+                        // }
+                        closestNeighbor = otherBird;
+                    }
+                }
+
+                updates2[bird] = closestNeighbor;
+            }
+
+            foreach (var update in updates)
+            {
+                _pigeons[update.Key] = update.Value;
+            }
+
+            foreach (var update in updates2)
+            {
+                _pigeons2[update.Key] = update.Value;
+            }
+            yield return new WaitForSeconds(1f);
         }
     }
+    
 }
